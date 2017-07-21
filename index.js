c0f4220e016f7585b68e35e9e93eae0727837116
Caleb Eby <caleb.eby01@gmail.com>
2017-07-20 20:41:40 -0700

    Add postcss

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 #!/usr/bin/env node
-const reshape = require('reshape')
+
 const fs = require('then-fs')
 const globby = require('globby')
+const path = require('path')
+const chalk = require('chalk')
+
+// Reshape
+const reshape = require('reshape')
 const sugarml = require('sugarml')
 const beautify = require('reshape-beautify')
-const path = require('path')
+
+// Postcss
+const postcss = require('postcss')
+const sugarss = require('sugarss')
 
 const reshapeConfig = {
   parser: sugarml,
   plugins: [beautify()]
 }
 
-const getFiles = () => globby('**/*.sgr')
-
-const processFile = file => {
-  const newFile = path.join(path.dirname(file), path.basename(file, 'sgr') + 'html')
-  fs.readFile(file, 'utf-8')
-    .then(text => reshape(reshapeConfig).process(text))
-    .then(res => res.output())
-    .then(newText => fs.writeFile(newFile, newText))
+const processFiles = opts => {
+  globby([`**/*.${opts.inputExt}`, '!node_modules'])
+    .then(files =>
+      Promise.all(
+        files.map(file => {
+          const newFile = path.join(
+            path.dirname(file),
+            path.basename(file, opts.inputExt) + opts.outputExt
+          )
+          return fs
+            .readFile(file, 'utf-8')
+            .then(opts.process)
+            .then(newText => fs.writeFile(newFile, newText))
+        })
+      )
+    )
+    .then(console.log(chalk.green('Done!')))
 }
 
-getFiles()
-  .then(files => Promise.all(files.map(processFile)))
-  .then(done =>  console.log(done))
+processFiles({
+  inputExt: 'sgr',
+  outputExt: 'html',
+  process: text =>
+    reshape(reshapeConfig).process(text).then(res => res.output())
+})
 
-// reshape({})
-//   .process(htmlInput)
-//   .then((res) => {
-//     res.output({ name: 'reshape' })
-//   })
-//   .then()
+processFiles({
+  inputExt: 'sss',
+  outputExt: 'css',
+  process: text => postcss().process(text, {parser: sugarss})
+})
